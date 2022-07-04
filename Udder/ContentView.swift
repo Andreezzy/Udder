e35fa658593b98bef4a3cc704bcd2392f8444bcd
Andres <frandres333@gmail.com>
2022-07-04 00:00:50 -0500

    Changes

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .padding()
             Text("MILK")
                 .padding()
+            Text("Cow")
+                .padding()
         }
         
     }
